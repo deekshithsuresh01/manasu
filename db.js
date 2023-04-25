3dbed94bf3acd176ac6f64b763b7aa26a488346f
Deekshith Suresh <111529947+deekshithsuresh01@users.noreply.github.com>
2023-04-25 12:57:30 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-const uri='mongodb+srv://loan21:loan21@cluster0.i6x39wy.mongodb.net/?retryWrites=true&w=majority';
+const uri='mongodb+srv://loan21:loan21@cluster0.i6x39wy.mongodb.net/udharify?retryWrites=true&w=majority';
 const connecttomongo=()=>{
     mongoose.connect(uri).then((data)=>{
         console.log('Connected to databse successfully '+ data.Connection.name)
